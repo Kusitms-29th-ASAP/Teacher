356f4d845ec0041ce190308b556064e6ecc6b912
yyypearl <hor629@naver.com>
2024-05-22 23:25:20 +0900

    feat(#9): axios 모듈

--- src/apis/authAxios.ts
+++ src/apis/authAxios.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 
-const Axios = axios.create({
+const token = localStorage.getItem("accessToken");
+
+const authAxios = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: {
+    Authorization: `Bearer ${token}`,
     "Content-Type": "application/json;charset=UTF-8",
   },
 });
-export default Axios;
+export default authAxios;
