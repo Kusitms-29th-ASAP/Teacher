d1dbb165ba905fee33c05c5449b68bc40062b446
yyypearl <hor629@naver.com>
2024-05-23 22:29:22 +0900

    feat(#13): 알림장 추가 API 연동

--- src/components/common/ListItem.tsx
+++ src/components/common/ListItem.tsx
@@ -2,46 +2,151 @@ import { theme } from "@/styles/theme";
 import React, { useState } from "react";
 import styled from "styled-components";
 import Checkbox from "./Checkbox";
-import Image from "next/image";
 import Category from "./Category";
+import Calendar from "./Calendar";
 
 const categoryClass = ["가정통신문", "준비물", "숙제", "기타"];
+type CategoryKey = "가정통신문" | "준비물" | "숙제" | "기타";
 
-const ListItem = () => {
-  const [todo, setTodo] = useState("");
+interface ListBoxItemProps {
+  description: string;
+  isLinkedWithTodo: boolean;
+  todoType: string;
+  deadline: string;
+  onChange: (
+    description: string,
+    isLinkedWithTodo: boolean,
+    todoType: string,
+    deadline: string
+  ) => void;
+}
+
+interface AnnouncementDetail {
+  description: string;
+  isLinkedWithTodo: boolean;
+  todoType: string;
+  deadline: string;
+}
+
+interface ListBoxComponentProps {
+  items: AnnouncementDetail[];
+  onChange: (updatedItems: AnnouncementDetail[]) => void;
+}
+
+const ListBoxItem: React.FC<ListBoxItemProps> = ({
+  description,
+  isLinkedWithTodo,
+  todoType,
+  deadline,
+  onChange,
+}) => {
+  const [isChecked, setIsChecked] = useState<boolean>(isLinkedWithTodo);
   const [selectedCategory, setSelectedCategory] = useState("");
 
+  const handleDescriptionChange = (event: any) => {
+    const newDescription = event.target.value;
+    onChange(newDescription, isLinkedWithTodo, todoType, deadline);
+  };
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+    onChange(description, !isChecked, todoType, deadline);
+  };
+
+  const categoryMap: Record<CategoryKey, string> = {
+    가정통신문: "NONE",
+    준비물: "SUPPLY",
+    숙제: "HOMEWORK",
+    기타: "ETC",
+  };
+
+  const handleCategoryChange = (selectedCategory: string) => {
+    const newTodoType = categoryMap[selectedCategory as CategoryKey] || "NONE";
+    setSelectedCategory(selectedCategory as CategoryKey);
+    onChange(description, isChecked, newTodoType, deadline);
+  };
+
+  const handleDeadlineChange = (value: string) => {
+    onChange(description, isChecked, todoType, value);
+  };
+
   return (
-    <ListBox>
+    <LineBox>
       <Description>
         <Input
-          value={todo}
-          onChange={(e) => setTodo(e.target.value)}
+          value={description}
+          onChange={handleDescriptionChange}
           placeholder="내용을 입력해주세요"
         />
       </Description>
-      <Checkbox />
+      <div>
+        <Checkbox checked={isChecked} onChange={handleCheckboxChange} />
+      </div>
       <Category
         selectedCategory={selectedCategory}
         setSelectedCategory={setSelectedCategory}
-        data={categoryClass}
+        data={Object.keys(categoryMap)}
+        onChange={handleCategoryChange}
       />
-      <DeadLine>
-        <Image
-          src="/assets/icons/ic_calender_orange.svg"
-          alt="calendar"
-          width={24}
-          height={24}
-          style={{ cursor: "pointer" }}
+      <Calendar value={deadline} onChange={handleDeadlineChange} />
+    </LineBox>
+  );
+};
+
+const ListBoxComponent: React.FC<ListBoxComponentProps> = ({
+  items,
+  onChange,
+}) => {
+  const handleItemChange = (
+    description: string,
+    isLinkedWithTodo: boolean,
+    todoType: string,
+    deadline: string
+  ) => {
+    const updatedItems = items.map((item) => {
+      if (item.description === description) {
+        return {
+          ...item,
+          isLinkedWithTodo,
+          todoType,
+          deadline,
+        };
+      }
+      return item;
+    });
+    onChange(updatedItems);
+  };
+
+  return (
+    <ListBox>
+      {items.map((item, index) => (
+        <ListBoxItem
+          key={index}
+          description={item.description}
+          isLinkedWithTodo={item.isLinkedWithTodo}
+          todoType={item.todoType}
+          deadline={item.deadline}
+          onChange={handleItemChange}
         />
-      </DeadLine>
+      ))}
     </ListBox>
   );
 };
 
-export default ListItem;
+export default ListBoxComponent;
 
 const ListBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 700px;
+  gap: 10px;
+  border-radius: 16px;
+  color: ${theme.colors.b400};
+  ${(props) => props.theme.fonts.body3_m};
+  background: ${theme.colors.white};
+`;
+
+const LineBox = styled.div`
   width: 100%;
   height: 52px;
   border-radius: 8px;
@@ -52,6 +157,7 @@ const ListBox = styled.div`
   align-items: center;
   gap: 30px;
 `;
+
 const Description = styled.div`
   height: 100%;
   padding: 13px 16px;
@@ -72,10 +178,4 @@ const Input = styled.input`
     color: ${theme.colors.b300};
     ${(props) => props.theme.fonts.body2_r};
   }
-`;
-
-const DeadLine = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  padding-right: 16px;
 `;
